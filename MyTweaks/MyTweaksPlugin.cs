4cc14193fea9d15c65e1fc71936d1c2f0e648923
Anonymous <>
2023-04-07 03:35:05 -0600

    add new tweaks

--- MyTweaks/MyTweaksPlugin.cs
+++ MyTweaks/MyTweaksPlugin.cs
@@ -105,5 +105,118 @@ namespace garfieldbanks.MonsterSanctuary.MyTweaks
                 return true;
             }
         }
+
+        [HarmonyPatch(typeof(MagicalVines), "CheckPlayerInteraction")]
+        private class MagicalVinesCheckPlayerInteractionPatch
+        {
+            [UsedImplicitly]
+            private static bool Prefix(ref bool __result)
+            {
+                __result = true;
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(InvisiblePlatform), "Update")]
+        private class InvisiblePlatformUpdatePatch
+        {
+            [UsedImplicitly]
+            private static bool Prefix(ref InvisiblePlatform __instance)
+            {
+                if (!(GameController.Instance == null))
+                {
+                    __instance.PlatformRoot.SetActive(true);
+                }
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(MountAbility), "StartAction")]
+        private class MountAbilityStartActionPatch
+        {
+            [UsedImplicitly]
+            private static bool Prefix()
+            {
+                MountAbility component = PlayerController.Instance.Follower.Monster.ExploreAction.GetComponent<MountAbility>();
+                if (component != null)
+                {
+                    component.TarMount = true;
+                }
+                return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(PlayerFollower), "CanUseAction")]
+        private class PlayerFollowerSetVisiblePatch
+        {
+            [UsedImplicitly]
+            private static bool Prefix()
+            {
+                FlyingAbility component = PlayerController.Instance.Follower.Monster.ExploreAction.GetComponent<FlyingAbility>();
+                if (component != null)
+                {
+                    component.IsImprovedFlying = true;
+                    component.DualSwimmingAbility = true;
+                }
+                return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(SwimmingAbility), "StartAction")]
+        private class SwimmingAbilityStartActionPatch
+        {
+            [UsedImplicitly]
+            private static bool Prefix()
+            {
+                SwimmingAbility component = PlayerController.Instance.Follower.Monster.ExploreAction.GetComponent<SwimmingAbility>();
+                if (component != null)
+                {
+                    component.ResistsStreams = true;
+                }
+                return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(DarkRoomLightManager), "Update")]
+        private class DarkRoomLightManagerUpdatePatch
+        {
+            [UsedImplicitly]
+            private static bool Prefix(ref DarkRoomLightManager __instance)
+            {
+                LightAbility component2 = PlayerController.Instance.Follower.Monster.ExploreAction.GetComponent<LightAbility>();
+                MountAbility component3 = PlayerController.Instance.Follower.Monster.ExploreAction.GetComponent<MountAbility>();
+                if (component2 == null && component3 == null || component3 != null && !component3.SonarMount)
+                {
+                    //MethodInfo LightenArea = __instance.GetType().GetMethod("LightenArea", BindingFlags.NonPublic | BindingFlags.Instance);
+
+                    // Light:
+                    //LightenArea.Invoke(__instance, new object[] { PlayerController.Instance.Follower.transform.position, 17, 2, 1f, __instance.LightColor, false });
+
+                    // Sonar:
+                    //LightenArea.Invoke(__instance, new object[] { PlayerController.Instance.Follower.transform.position, 16, 8, 1f, (Color) new Color32(255, 0, 1, 0), true });
+                    return false;
+                }
+                //else if (component2 != null)
+                //{
+                //    _log.LogDebug($"Name: {PlayerController.Instance.Follower.Monster.GetName()}");
+                //    _log.LogDebug($"component2.TotalRadius: {component2.TotalRadius}");
+                //    _log.LogDebug($"component2.FalloffRadius: {component2.FalloffRadius}");
+                //    _log.LogDebug($"__instance.LightColor: {__instance.LightColor}");
+                //    _log.LogDebug($"component2.Color: {component2.Color}");
+                //    _log.LogDebug($"__instance.LightColor * component2.Color: {__instance.LightColor * component2.Color}");
+                //    _log.LogDebug($"component2.OnlyApplyOnDarkAreas: {component2.OnlyApplyOnDarkAreas}");
+                //}
+                //else if (component3 != null && component3.SonarMount)
+                //{
+                //    _log.LogDebug($"Name: {PlayerController.Instance.Follower.Monster.GetName()}");
+                //    _log.LogDebug($"component3.SonarTotalRadius: {component3.SonarTotalRadius}");
+                //    _log.LogDebug($"component3.SonarFalloffRadius: {component3.SonarFalloffRadius}");
+                //    _log.LogDebug($"__instance.LightColor: {__instance.LightColor}");
+                //    _log.LogDebug($"component3.Color: {component3.SonarColor}");
+                //    _log.LogDebug($"__instance.LightColor * component3.Color: {__instance.LightColor * component3.SonarColor}");
+                //}
+                return true;
+            }
+        }
     }
 }
